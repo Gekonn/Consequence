bc3031e2b5d3f7b2c2f6e199717633ce07e5ed20
Petr Palas <Petr Palas>
2016-09-06 11:28:41 +0200

    basic UI work

---
+++ Assets/Source/Core.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Source
+{
+    public class Core : MonoBehaviour
+    {
+        public static Core Instance;
+
+        void Awake()
+        {
+            //incialize app core
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (Instance != this)
+            {
+                DestroyImmediate(gameObject);
+            }
+
+            Application.targetFrameRate = 60;
+        }
+    }
+}
